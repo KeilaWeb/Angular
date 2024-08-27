abfa5fc344e065e683e791ecdf5606781b069ac5
KeilaWeb <keilakdbweb@gmail.com>
2024-08-27 14:56:32 -0300

    maked angular storybook

---
+++ storybook/src/app/components/card/card.stories.ts
@@ -0,0 +1,25 @@
+import { Meta, StoryObj } from '@storybook/angular';
+import { CardComponent } from './card.component';
+
+export default {
+  title: 'Componentes/MyCard',
+  component: CardComponent,
+} as Meta<CardComponent>;
+
+export const Purple: StoryObj<CardComponent> = {
+  args: {
+    cardStyle: 'purple',
+  }
+}
+
+export const Orange: StoryObj<CardComponent> = {
+  args: {
+    cardStyle: 'orange',
+  }
+}
+
+export const Blue: StoryObj<CardComponent> = {
+  args: {
+    cardStyle: 'blue',
+  }
+}
