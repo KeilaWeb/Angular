2d404a86b57d77beae68397b5f8004fda79378d6
KeilaWeb <keilakdbweb@gmail.com>
2024-08-29 13:45:55 -0300

    created initial class dinamic user list

---
+++ lista-de-usuarios-filtro/src/app/angular-material/angular-material.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from "@angular/core";
+
+import { MatButtonModule } from "@angular/material/button";
+import {MatTableModule} from '@angular/material/table';
+import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatNativeDateModule } from "@angular/material/core";
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+
+@NgModule({
+  imports: [
+    // MatButtonModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatDividerModule,
+    MatSelectModule,
+    MatInputModule,
+    MatTableModule,
+    MatListModule
+  ],
+  exports: [
+    // MatButtonModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatDividerModule,
+    MatSelectModule,
+    MatInputModule,
+    MatTableModule,
+    MatListModule
+  ],
+})
+export class AngularMaterialModule { }
---
+++ lista-de-usuarios-filtro/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ComponentsModule } from './components/components.module';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ComponentsModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [
+    provideClientHydration(),
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
